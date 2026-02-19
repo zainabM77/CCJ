bf8e13b2e3f62a00fa5152abc7d2acf017c97414
zainabM77 <zmujadidi@torontomu.ca>
2026-02-18 23:38:50 -0500

    adding updates

--- script.js
+++ script.js
@@ -4,5 +4,17 @@ const menuLinks = document.querySelector('.navbar__menu')
 menu.addEventListener('click', function (){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
+});
 
+    document.addEventListener("DOMContentLoaded", function () {
+  const amountSelect = document.getElementById("amountSelect");
+  const customAmountGroup = document.getElementById("customAmountGroup");
+
+  amountSelect.addEventListener("change", function () {
+    if (this.value === "Other") {
+      customAmountGroup.style.display = "block";
+    } else {
+      customAmountGroup.style.display = "none";
+    }
+  });
 });
