9359c52a7540995e4ae53ca767f31befd0581d54
zainabM77 <zmujadidi@torontomu.ca>
2026-03-15 14:38:16 -0400

    fixing join team form

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const menu = document.querySelector ('#mobile-menu')
+const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
 menu.addEventListener('click', function (){
@@ -6,15 +6,33 @@ menu.addEventListener('click', function (){
     menuLinks.classList.toggle('active');
 });
 
-    document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
+
   const amountSelect = document.getElementById("amountSelect");
   const customAmountGroup = document.getElementById("customAmountGroup");
 
-  amountSelect.addEventListener("change", function () {
-    if (this.value === "Other") {
-      customAmountGroup.style.display = "block";
-    } else {
-      customAmountGroup.style.display = "none";
-    }
-  });
+  if(amountSelect){
+    amountSelect.addEventListener("change", function () {
+      if (this.value === "Other") {
+        customAmountGroup.style.display = "block";
+      } else {
+        customAmountGroup.style.display = "none";
+      }
+    });
+  }
+
+  // COMMUNITY MEMBER FEE DISPLAY
+  const positionSelect = document.getElementById("positionSelect");
+  const feeNotice = document.getElementById("membershipFeeNotice");
+
+  if(positionSelect){
+    positionSelect.addEventListener("change", function () {
+      if (this.value === "Community Member") {
+        feeNotice.style.display = "block";
+      } else {
+        feeNotice.style.display = "none";
+      }
+    });
+  }
+
 });
